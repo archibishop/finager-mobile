784c979d8210f543f822f917c1c30e27343ac80b
Wagubi Brian <wagubib@gmail.com>
2018-11-19 10:59:04 +0300

    Setup jacoco for unified test coverage report

---
+++ app/src/androidTest/java/com/example/wagubibrian/finager_mobile_android/MainActivityInstrumentedTest.java
@@ -0,0 +1,33 @@
+package com.example.wagubibrian.finager_mobile_android;
+
+        import android.content.Context;
+        import android.support.test.InstrumentationRegistry;
+        import android.support.test.rule.ActivityTestRule;
+        import android.support.test.runner.AndroidJUnit4;
+
+        import org.junit.Rule;
+        import org.junit.Test;
+        import org.junit.runner.RunWith;
+
+        import static android.support.test.espresso.Espresso.onView;
+        import static android.support.test.espresso.assertion.ViewAssertions.matches;
+        import static android.support.test.espresso.matcher.ViewMatchers.withId;
+        import static android.support.test.espresso.matcher.ViewMatchers.withText;
+        import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class MainActivityInstrumentedTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> rule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void showTextHelloWorld() {
+        onView(withId(R.id.text_view)).check(matches(withText("Hello World!")));
+    }
+}
